dd657e0be64d1961cd150263a452d6a4d702957f
Kacper Rączy <gfw.kra@gmail.com>
2022-10-02 14:40:56 +0200

    Multi sheet support

--- Sources/locgen-swift/ParserXLSX.swift
+++ Sources/locgen-swift/ParserXLSX.swift
@@ -5,89 +5,73 @@ import CoreXLSX
 import Yams
 
 class ParserXLSX {
-    
-    private let mainKey = "key-ios"
-    private let commentKey = "comment"
-    private let languagesKey = "languages"
-    private let dirsKey = "dirs"
-    private let namesKey = "names"
-    
+    private let sheets: [String]
     private var fileContent: [URL: String] = [:]
     private var columns: Set<String> = []
     
-    func parse(xlsxData: Data, mapData: Data) throws {
-        let map = try processMap(mapData: mapData)
+    init(sheets: [String]) {
+        self.sheets = sheets
+    }
+    
+    func parse(xlsxData: Data, config: Config) throws {
         let file = try  XLSXFile(data: xlsxData)
         
-        try proceed(file: file, map: map)
+        try proceed(file: file, config: config)
     }
     
-    private func processMap(mapData: Data) throws -> [String: Any] {
-        if let yamlString = String(bytes: mapData, encoding: .utf8) {
-            if let yaml = try Yams.load(yaml: yamlString) as? [String: Any] {
-                return yaml
-            } else {
-                throw InternalError.cantConvert("\n\(yamlString)\n")
-            }
-        } else {
-            throw InternalError.cantConvert("Map data")
-        }
-    }
-    
-    private func proceed(file: XLSXFile, map: [String: Any]) throws {
+    private func proceed(file: XLSXFile, config: Config) throws {
         let workbooks = try file.parseWorkbooks()
         
         guard !workbooks.isEmpty else {
             throw InternalError.emptyArray("No workbooks in XLSX file")
         }
-    
-        var selectedWorksheet: Worksheet?
+
+        let sheetNames = Set(sheets)
+        var selectedWorksheets: [(String, Worksheet)] = []
         for workbook in workbooks {
             for (name, path) in try file.parseWorksheetPathsAndNames(workbook: workbook) {
-                if let selectedSheetName = LocgenOptions.shared.sheet {
-                    if name == selectedSheetName {
-                        selectedWorksheet = try file.parseWorksheet(at: path)
-                    }
-                    
+                guard let name = name else {
+                    continue
+                }
+                
+                guard sheetNames.contains(name) || sheetNames.isEmpty else {
+                    continue
+                }
+                
+                selectedWorksheets.append((name, try file.parseWorksheet(at: path)))
+                if sheetNames.isEmpty {
                     break
                 }
             }
         }
         
-        guard let worksheet = selectedWorksheet else {
-            throw InternalError.cantFind("Worksheet with name: `\(LocgenOptions.shared.sheet ?? "")`")
+        if sheetNames.isEmpty && selectedWorksheets.isEmpty {
+            throw InternalError.cantFind("Any worksheet")
+        } else if !sheetNames.isEmpty && selectedWorksheets.count != sheetNames.count {
+            throw InternalError.cantFind("Worksheets with names: `\(sheets)`")
         }
 
         let strings =  try file.parseSharedStrings()
         
-        try proceed(worksheet: worksheet, map: map, sharedStrings: strings)
+        for (sheetName, worksheet) in selectedWorksheets {
+            try proceed(worksheet: worksheet, worksheetName: sheetName, config: config, sharedStrings: strings)
+        }
     }
     
-    private func proceed(worksheet: Worksheet, map: [String: Any], sharedStrings: SharedStrings?) throws {
+    private func proceed(worksheet: Worksheet, worksheetName: String, config: Config, sharedStrings: SharedStrings?) throws {
         let workingDirectoryURL = try FileManager.default.currentDirectoryPath.asFileURL()
         
         guard let data = worksheet.data else {
-            throw InternalError.emptyData("No data for worksheet `\(LocgenOptions.shared.sheet ?? "")`")
+            throw InternalError.emptyData("No data for worksheet `\(worksheetName)`")
         }
         
         guard let headerRow = data.rows.first else {
-            throw InternalError.emptyData("No rows to read in \(LocgenOptions.shared.sheet ?? "") sheet")
+            throw InternalError.emptyData("No rows to read in \(worksheetName) sheet")
         }
 
-        guard let mainKeyMap = map[mainKey] as? String else {
-            throw InternalError.missingParam("Can't find `key-ios` key in map (yml) file")
-        }
-        
-        guard let languagesMap = map[languagesKey] as? [String: String] else {
-            throw InternalError.missingParam("Can't find `languages` key in map (yml) file")
-        }
-        
-        guard !languagesMap.isEmpty else {
+        guard !config.languages.isEmpty else {
             throw InternalError.emptyArray("map (yml) `languages` can't be empty, should have at least one pair")
         }
-        
-        let dirsMap = map[dirsKey] as? [String: String]
-        let namesMap = map[namesKey] as? [String: String]
    
         var mainKeyRef: ColumnReference? = nil
         var languageColumns: [String: ColumnReference] = [:]
@@ -96,24 +80,24 @@ class ParserXLSX {
             let value = extractValue(from: cell, sharedStrings: sharedStrings)
             
             if let value = value {
-                if value == mainKeyMap {
+                if value == config.key {
                     mainKeyRef = cell.reference.column
-                } else if let object = languagesMap.first(where: {_, val in return val == value }) {
+                } else if let object = config.languages.first(where: {_, val in return val == value }) {
                     languageColumns[object.key] = cell.reference.column
                 }
             }
         }
         
         guard let mainKeyRef = mainKeyRef else {
-            throw InternalError.cantFind("Can't find column named `\(mainKeyMap)` in xlsx file")
+            throw InternalError.cantFind("Can't find column named `\(config.key)` in xlsx file")
         }
         
-        let languagesDiff = Set(languagesMap.keys).symmetricDifference(Set(languageColumns.keys))
+        let languagesDiff = Set(config.languages.keys).symmetricDifference(Set(languageColumns.keys))
         guard languagesDiff.count == .zero else {
-            let missingValues = languagesDiff.compactMap { languagesMap[$0] }
+            let missingValues = languagesDiff.compactMap { config.languages[$0] }
             throw InternalError.cantFind("Can't find languages `\(missingValues)` in xlsx file")
         }
-                
+        
         var isFirstRow = true
         for i in 1..<data.rows.count {
             let row = data.rows[i]
@@ -128,8 +112,8 @@ class ParserXLSX {
                 sharedStrings: sharedStrings,
                 languageMap: languageColumns,
                 isFirstRow: isFirstRow,
-                dirsMap: dirsMap,
-                namesMap: namesMap,
+                dirsMap: config.dirs,
+                namesMap: config.filenames,
                 workingDir: workingDirectoryURL
             )
             
@@ -164,9 +148,7 @@ class ParserXLSX {
         namesMap: [String: String]?,
         workingDir: URL
     ) throws {
-        
         for (lang, columnRef) in languageMap {
-          
             guard
                 let cell = row.cells.first(where: { cell in return cell.reference.column.value == columnRef.value }),
                 let translation = extractValue(from: cell, sharedStrings: sharedStrings)
